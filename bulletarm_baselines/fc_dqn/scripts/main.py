6848a1025bbed637b34a8cf0b344e189a49ba899
tingxi <tingxi@x86mbp.local>
2023-09-16 20:42:53 +0800

    default commit message

--- bulletarm_baselines/fc_dqn/scripts/main.py
+++ bulletarm_baselines/fc_dqn/scripts/main.py
@@ -173,8 +173,9 @@ def train():
 
                 ###2
                 planner_actions_star = planner_actions_star.to(device)
-                ###2
                 print(planner_actions_star.shape, states.shape)
+                ###2
+                
                 planner_actions_star = torch.cat((planner_actions_star, states.unsqueeze(1)), dim=1)
                 states_, in_hands_, obs_, rewards, dones = planner_envs.step(planner_actions_star, auto_reset=True)
                 buffer_obs = getCurrentObs(in_hands, obs)
