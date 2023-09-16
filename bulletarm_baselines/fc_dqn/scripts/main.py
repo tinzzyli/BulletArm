2a6e764e02eb1133877b26358d78c30eb1cbb8fb
tingxi <tingxi@x86mbp.local>
2023-09-16 20:42:35 +0800

    default commit message

--- bulletarm_baselines/fc_dqn/scripts/main.py
+++ bulletarm_baselines/fc_dqn/scripts/main.py
@@ -162,12 +162,19 @@ def train():
             local_transitions = [[] for _ in range(planner_num_process)]
             while j < planner_episode:
                 plan_actions = planner_envs.getNextAction()
+
+                ###1
                 plan_actions = plan_actions.to(device)
                 if num_processes == 0:
                     plan_actions = plan_actions.unsqueeze(dim=0)
-                
+                ###1
 
                 planner_actions_star_idx, planner_actions_star = agent.getActionFromPlan(plan_actions)
+
+                ###2
+                planner_actions_star = planner_actions_star.to(device)
+                ###2
+                print(planner_actions_star.shape, states.shape)
                 planner_actions_star = torch.cat((planner_actions_star, states.unsqueeze(1)), dim=1)
                 states_, in_hands_, obs_, rewards, dones = planner_envs.step(planner_actions_star, auto_reset=True)
                 buffer_obs = getCurrentObs(in_hands, obs)
