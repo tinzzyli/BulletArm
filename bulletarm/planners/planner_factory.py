895e7ea37af2855729ecea439fc7a435cfbb0933
tingxi <tingxi@x86mbp.local>
2023-09-17 01:48:59 +0800

    default commit message

--- bulletarm/planners/planner_factory.py
+++ bulletarm/planners/planner_factory.py
@@ -16,8 +16,6 @@ def getPlannerFn(env_type, planner_config):
 
   if planner_type in constants.PLANNERS:
     # return lambda env: constants.PLANNERS[planner_type](env, planner_config)
-    # def planner_fn(env):
-    #     return constants.PLANNERS[planner_type](env, planner_config)
-    return planner_fn
+    return lambda env: planner_fn(env, planner_type, planner_config)
   else:
     raise ValueError('Invalid planner passed to factory.')
