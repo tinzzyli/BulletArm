640e12ea8226f36e2ef56d539e057eb810b4562c
tingxi <tingxi@x86mbp.local>
2023-09-17 21:43:25 +0800

    default commit message

--- bulletarm/pybullet/utils/sensor.py
+++ bulletarm/pybullet/utils/sensor.py
@@ -44,6 +44,8 @@ class Sensor(object):
 
     self.device = device
 
+    pyredner.set_use_gpu(False)
+
   def setCamMatrix(self, cam_pos, cam_up_vector, target_pos):
     self.view_matrix = pb.computeViewMatrix(
       cameraEyePosition=[cam_pos[0], cam_pos[1], cam_pos[2]],
@@ -61,7 +63,7 @@ class Sensor(object):
     # dir = "./bulletarm/pybullet/urdf/object/GraspNet1B_object/"+object_index+"/convex.obj"
     dir = "./bulletarm/pybullet/urdf/object/GraspNet1B_object/055/convex.obj"
 
-    o = pyredner.load_obj(dir, return_objects=True, device=torch.device("cpu"))
+    o = pyredner.load_obj(dir, return_objects=True)
     new_obj = o[0]
 
     orien = self.objs[0].getRotation()
@@ -115,7 +117,7 @@ class Sensor(object):
                         )
     scene = pyredner.Scene(camera = camera, objects = obj_list)
     chan_list = [pyredner.channels.depth]
-    depth_img = pyredner.render_generic(scene, chan_list,device=torch.device("cpu"))
+    depth_img = pyredner.render_generic(scene, chan_list)
     near = 0.09
     far = 0.010
     depth = near * far /(far - depth_img)
@@ -135,7 +137,7 @@ class Sensor(object):
     rendering_list, _, _ = self.importSingleObject(scale=self.scale)
 
     tray_dir = "./tray.obj"
-    t = pyredner.load_obj(tray_dir, return_objects=True, device=torch.device("cpu"))
+    t = pyredner.load_obj(tray_dir, return_objects=True)
     tray = t[0]   
     tray.vertices = tray.vertices.to(self.device)
     tray.vertices /= 1000
@@ -166,7 +168,7 @@ class Sensor(object):
     self.size = size      
     rendering_list, ORI_OBJECT_LIST, params = self.importSingleObject(scale=self.scale)
     tray_dir = "./tray.obj"
-    t = pyredner.load_obj(tray_dir, return_objects=True, device=torch.device("cpu"))
+    t = pyredner.load_obj(tray_dir, return_objects=True)
     tray = t[0]   
     tray.vertices /= 1000
     tray.vertices[:,0:1] +=  0.5
