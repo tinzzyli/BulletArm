0dba6800fd10cd21222e07d14b3e08dd5aa7c3d6
Tinzzy Li <tinzzy.li@gmail.com>
2023-11-02 14:36:27 +0800

    commit

--- tutorials/block_stacking_demo.py
+++ tutorials/block_stacking_demo.py
@@ -2,7 +2,7 @@ from bulletarm import env_factory
 
 def runDemo():
   env_config = {'render': True, 'object_index': -1}
-  env = env_factory.createEnvs(0, 'house_building_4', env_config)
+  env = env_factory.createEnvs(0, 'house_building_3', env_config)
   obs = env.reset()
   done = False
   while not done:
