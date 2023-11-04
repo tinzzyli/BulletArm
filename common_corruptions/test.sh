a9167517862aa0167b29abcca167c092c2ef4d93
Tinzzy Li <tinzzy.li@gmail.com>
2023-11-05 01:28:59 +0800

    default commit message

--- common_corruptions/test.sh
+++ common_corruptions/test.sh
@@ -4,6 +4,6 @@ values=("gaussian_noise" "shot_noise" "impulse_noise" "motion_blur" "zoom_blur" 
 
 for value in "${values[@]}";do
 {
-    !python ./common_corruptions/corruption_test.py  --device_name=cuda --env=house_building_3 --corrupt_func="$value" --severity=5 --load_model_pre=/content/drive/MyDrive/my_archive/house_building_3/snapshot
+    python ./common_corruptions/corruption_test.py  --device_name=cuda --env=house_building_3 --corrupt_func="$value" --severity=5 --load_model_pre=/content/drive/MyDrive/my_archive/house_building_3/snapshot
 }
 done
