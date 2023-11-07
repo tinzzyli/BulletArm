986d7db081df0800bc70a622e38848da69152e02
Tinzzy Li <tinzzy.li@gmail.com>
2023-11-07 13:16:29 +0800

    m

--- common_corruptions/test.sh
+++ common_corruptions/test.sh
@@ -5,5 +5,6 @@ values=("gaussian_noise" "poisson_noise" "salt_pepper_noise", "rotation", "trans
 for value in "${values[@]}";do
 {
     python ./common_corruptions/corruption_test.py  --device_name=cuda --env=house_building_3 --corrupt_func="$value" --severity=5 --load_model_pre=/content/drive/MyDrive/my_archive/house_building_3/snapshot
+    python ./common_corruptions/corruption_test.py  --device_name=cuda --env=house_building_3 --corrupt_func="$value" --severity=5 --load_model_pre=/content/drive/MyDrive/my_archive/house_building_3/snapshot
 }
 done
