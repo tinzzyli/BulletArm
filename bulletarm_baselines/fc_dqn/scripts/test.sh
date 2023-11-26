9a9c3a440e3dd11de4c610f7ad78d6df4e2d4658
Tinzzy Li <tinzzy.li@gmail.com>
2023-11-26 15:22:22 +0800

    m

--- bulletarm_baselines/fc_dqn/scripts/test.sh
+++ bulletarm_baselines/fc_dqn/scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for ((i=0;i<10;i++));do
+for ((i=10;i<20;i++));do
 
 {
     python bulletarm_baselines/fc_dqn/scripts/pgd_attack.py --device_name=cuda --num_process=0 --load_model_pre="/content/drive/MyDrive/my_archive/file/ck5/snapshot" --num_object=1 --object_index=${i}
