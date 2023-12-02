f2244b9e98ebc7b768516692e01cdfdeeb619a8f
Tinzzy Li <tinzzy.li@gmail.com>
2023-12-02 21:44:34 +0800

    86

--- bulletarm_baselines/fc_dqn/scripts/grid_test.sh
+++ bulletarm_baselines/fc_dqn/scripts/grid_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for ((i=0;i<10;i++));do
+for ((i=0;i<86;i++));do
 
 {
     python bulletarm_baselines/fc_dqn/scripts/grid_test.py --device_name=cuda --num_process=0 --load_model_pre="/root/snapshot" --num_object=1 --object_index=${i}
