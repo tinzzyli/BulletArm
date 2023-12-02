7be37dfd128b4957e2639a3fc9d412d4e5b08e48
Tinzzy Li <tinzzy.li@gmail.com>
2023-12-02 17:49:47 +0800

    grid sh

--- bulletarm_baselines/fc_dqn/scripts/grid_test.sh
+++ bulletarm_baselines/fc_dqn/scripts/grid_test.sh
@@ -2,7 +2,7 @@
 for ((i=0;i<10;i++));do
 
 {
-    python bulletarm_baselines/fc_dqn/scripts/grid_test.py --device_name=cuda --num_process=0 --load_model_pre="/content/drive/MyDrive/my_archive/file/ck5/snapshot" --num_object=1 --object_index=${i}
+    python bulletarm_baselines/fc_dqn/scripts/grid_test.py --device_name=cuda --num_process=0 --load_model_pre="./snapshot" --num_object=1 --object_index=${i}
     
 }
 done
