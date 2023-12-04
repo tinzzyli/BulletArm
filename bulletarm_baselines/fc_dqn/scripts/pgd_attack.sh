f027d7079dc4aedbce95ddace49cc49d1d42b6cc
Tinzzy Li <tinzzy.li@gmail.com>
2023-12-05 02:33:18 +0800

    try 5

--- bulletarm_baselines/fc_dqn/scripts/pgd_attack.sh
+++ bulletarm_baselines/fc_dqn/scripts/pgd_attack.sh
@@ -3,7 +3,7 @@
 for ((i=0;i<86;i++));do
 
 {
-    python bulletarm_baselines/fc_dqn/scripts/pgd_attack.py --device_name=cuda --num_process=0 --load_model_pre="/content/drive/MyDrive/my_archive/snapshot" --num_object=1 --object_index=${i}
+    python bulletarm_baselines/fc_dqn/scripts/pgd_attack.py --device_name=cuda --num_process=0 --load_model_pre="/root/snapshot" --num_object=1 --object_index=${i}
     
 }
 done
