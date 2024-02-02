80ed9eca63729e354bcfa623644b2e6ce01d8215
Tinzzy Li <tinzzy.li@gmail.com>
2024-02-02 20:37:55 +0800

    m

---
+++ bulletarm_baselines/fc_dqn/scripts/loss_check.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for ((i=0;i<86;i++));do
+
+{
+    python bulletarm_baselines/fc_dqn/scripts/loss_check.py --device_name=cuda --num_process=0 --load_model_pre="/root/snapshot" --num_object=1 --object_index=${i}
+    
+}
+done
