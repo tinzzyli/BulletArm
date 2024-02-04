6c4fc1ad70d6cafff25e82b53b093d783fcfa8dc
Tinzzy Li <tinzzy.li@gmail.com>
2024-02-04 11:39:33 +0800

    difference check

--- tutorials/block_stacking_demo.py
+++ tutorials/block_stacking_demo.py
@@ -5,10 +5,11 @@ def runDemo():
   env_config = {'render': True, 'num_objects': 1,'object_index': 20}
   env = env_factory.createEnvs(0, 'object_grasping', env_config)
   
-  _, _, _, _, params = env._resetAttack(np.array([0.5258, 0.0218])) 
+  position = np.array([0.5258, 0.0218])
+  _, _, _, _, params = env._resetAttack(position) 
   
   a, _rot_mat_list, _scale_list = params
-  print(a)
+  # print(a)
   
   # _ = env.stepAttack(np.array([0., 0.6, 0.1, 5.49776703]))
   # _, _, _, _, _params = env._resetAttack(np.array([0.5434, -0.0411])) 
@@ -19,7 +20,9 @@ def runDemo():
   while not done:
     action = env.getNextAction()
     obs, reward, done = env.step(action)
-    print(action)
+    print(action, position)
+    print("distance: ", np.sqrt((action[1]-position[0])**2 + (action[2]-position[1])**2))
+    
 
   env.close()
 
