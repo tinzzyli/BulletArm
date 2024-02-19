5e184e30db25a20d63b3077ab5858d1688dcb92a
Tinzzy Li <tinzzy.li@gmail.com>
2024-02-19 12:44:48 +0800

    m

--- tutorials/block_stacking_demo.py
+++ tutorials/block_stacking_demo.py
@@ -2,10 +2,10 @@ from bulletarm import env_factory
 import time
 import numpy as np
 def runDemo():
-  env_config = {'render': True, 'num_objects': 1,'object_index': 38}
+  env_config = {'render': True, 'num_objects': 1,'object_index': 83}
   env = env_factory.createEnvs(0, 'object_grasping', env_config)
   
-  position = np.array([0.5203, 0.0075])
+  position = np.array([0.5083, -0.0393])
   _, _, _, _, params = env._resetAttack(position) 
   
   a, _rot_mat_list, _scale_list = params
@@ -19,7 +19,8 @@ def runDemo():
   done = False
   while not done:
     action = env.getNextAction()
-    action = np.array([0., 0.4824, 0.0323, 5.49779643])
+    # action = np.array([0., 0.4824, 0.0323, 5.49779643])
+    action = np.array([0., 0.5031, -0.0438, 5.49771161])
     obs, reward, done = env.step(action)
     print(action, position)
     print("distance: ", np.sqrt((action[1]-position[0])**2 + (action[2]-position[1])**2))
