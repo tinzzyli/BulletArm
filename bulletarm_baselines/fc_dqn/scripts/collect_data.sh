ce1d0cbfd3e603a474bb8ac3d6933cce295f4484
Tinzzy Li <tinzzy.li@gmail.com>
2024-04-17 18:45:23 +0800

    collect2

--- bulletarm_baselines/fc_dqn/scripts/collect_data.sh
+++ bulletarm_baselines/fc_dqn/scripts/collect_data.sh
@@ -3,7 +3,7 @@
 for ((i=0;i<86;i++));do
 
 {
-    python bulletarm_baselines/fc_dqn/scripts/collect_data.py --device_name=cuda --num_process=0 --load_model_pre="/root/snapshot" --num_object=1 --object_index=${i}
+    python bulletarm_baselines/fc_dqn/scripts/collect_data.py --device_name=cuda --num_process=0 --load_model_pre="/home/weilab/tingxi/snapshot" --num_object=1 --object_index=${i}
     
 }
 done
